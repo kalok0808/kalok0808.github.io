5f09cc90973a69e051b0d989fa1c5515398736f0
kalok0808 <86389374+kalok0808@users.noreply.github.com>
2021-10-05 12:03:58 +0800

    add

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+const menuToggle = document.querySelector(".toggle");
+const showcase = document.querySelector(".showcase");
 
-const menuToggle = document.querySelector('.toggle')
-const showcase = document.querySelector('.showcase')
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
+  showcase.classList.toggle("active");
+});
 
-menuToggle.addEventListener('click', () => {
-    menuToggle.classList.toggle('active')
-    showcase.classList.toggle('active')
-}) 
+let type = new Typed(".typing-text", {
+  strings: ["happy", "sadness", "relax"],
+  typeSpeed: 120,
+  loop: true,
+});
